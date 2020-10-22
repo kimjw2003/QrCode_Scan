4c98da233867910f3fd75f8d24ed6cd977824d78
kimjw2003 <kimjw111003@gmail.com>
2020-10-22 11:24:14 +0900

    [ADD] 라이브러리, scanBtn

--- app/src/main/java/com/example/qrcode_scan/MainActivity.kt
+++ app/src/main/java/com/example/qrcode_scan/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.qrcode_scan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+
+    fun startBarcodeLeader(view: View){
+        IntentIntegrator(this).initiateScan()
+
     }
 }
